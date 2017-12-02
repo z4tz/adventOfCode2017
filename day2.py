b4d8769993c579f4147183389b91745262953de6
z4tz <z4tz87@gmail.com>
2017-12-02 10:45:25 +0100

    day 2 solution

---
+++ day2.py
@@ -0,0 +1,31 @@
+from InputReader import read
+
+def calcChecksum(matrix):
+    checksum = 0
+    for row in matrix:
+        numbers = [int(numString) for numString in row.split('\t')]
+        checksum += max(numbers) - min(numbers)
+    return checksum
+
+
+def calcChecksumB(matrix):
+    checksum = 0
+    for row in matrix:
+        checksum += findEvenDivision([int(numString) for numString in row.split('\t')])
+    return checksum
+
+
+def findEvenDivision(numbers):
+    for i in xrange(0,len(numbers)):
+        for j in xrange(0,len(numbers)):
+            if i != j and numbers[i] % numbers[j] == 0:
+                return numbers[i]/numbers[j]
+
+
+def run(data):
+
+    print calcChecksum(data)
+    print calcChecksumB(data)
+
+if __name__ == "__main__":
+    run(read('inputs/day2.txt'))
