d0d359cef7901f84f1c76aab68a4e0d7a5c9f855
z4tz <z4tz87@gmail.com>
2017-12-02 09:52:38 +0100

    day one solution

---
+++ InputReader.py
@@ -0,0 +1,5 @@
+def read(filename):
+
+    with open(filename) as f:
+        lines = f.readlines()
+    return lines
---
+++ day1.py
@@ -0,0 +1,28 @@
+from InputReader import read
+
+
+def solveCaptcha(captcha):
+    numberList = captcha + captcha[0]
+    captchaSum = 0
+    for i in xrange(0,len(numberList)-1):
+        if numberList[i] == numberList[i+1]:
+            captchaSum += int(numberList[i])
+    return captchaSum
+
+
+def solveCaptchaB(captcha):
+    captchaSum = 0
+    for A, B in zip(captcha[:len(captcha)/2-1], captcha[len(captcha)/2:]):
+        if A == B:
+           captchaSum += int(A)
+    return captchaSum*2
+
+
+def run():
+    data = read('inputs/day1.txt')
+    print solveCaptcha(data[0])
+    print solveCaptchaB(data[0])
+
+
+if __name__ == "__main__":
+    run()
