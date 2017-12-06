eecc87dc5fa5d00425f11357de2dca02990ffbea
Jimmy Malmqvist <jimmy.malmqvist@maxiv.lu.se>
2017-12-06 16:21:10 +0100

    refactoring

--- day6.py
+++ day6.py
@@ -4,12 +4,10 @@ from InputReader import read
 def findLoop(memoryBanks):
     memoryBanks = [int(numString) for numString in memoryBanks.split()]
     history = []
-    loops = 0
     while memoryBanks not in history:
-        loops += 1
         history.append(memoryBanks[:])
         redistribute(memoryBanks)
-    return [loops, len(history) - history.index(memoryBanks)]
+    return [len(history), len(history) - history.index(memoryBanks)]
 
 
 def redistribute(memoryBanks):
