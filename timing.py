d835f2b32238a01922250d92c10a63047e8ff7a6
Jimmy Malmqvist <jimmy.malmqvist@maxiv.lu.se>
2017-12-08 13:35:04 +0100

    Added support for multiple runs for better timing

--- timing.py
+++ timing.py
@@ -2,7 +2,7 @@ import timeit
 import os
 
 days = xrange(1, len(os.listdir('inputs/'))+1)
-
+runs = 1
 
 def setupstring(day):
     return """
@@ -14,4 +14,4 @@ data = read('inputs/day{0}.txt')""".format(day)
 if __name__ == '__main__':
     for day in days:
         print("-----## Assignment day {0} ##-----".format(day))
-        print("Time used for assignment {0}: {1}s\n\n".format(day, timeit.timeit("run(data)", setup=setupstring(day), number=1)))
+        print("Time used for assignment {0}: {1}s\n\n".format(day, timeit.timeit("run(data)", setup=setupstring(day), number=runs)/runs))
