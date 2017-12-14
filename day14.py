6a64a24fdc839dfac65ac44c1605c809dbe0cc57
Jimmy Malmqvist <jimmy.malmqvist@maxiv.lu.se>
2017-12-14 15:33:04 +0100

    Added solution for day 14

---
+++ day14.py
@@ -0,0 +1,57 @@
+from InputReader import read
+from day10 import knotHashB as knotHash
+import numpy as np
+
+
+def usedSpace(data):
+    used = 0
+    for i in xrange(128):
+        key = data + '-{0}'.format(i)
+        binaryHash = [bin(int(char, 16))[2:].zfill(4) for char in knotHash(key)]
+        used += ''.join(binaryHash).count('1')
+    return used
+
+
+def findRegions(data):
+    binaryLists = []
+    for i in xrange(128):
+        key = data + '-{0}'.format(i)
+        binaryHash = ''.join([bin(int(char, 16))[2:].zfill(4) for char in knotHash(key)])
+        binaryLists.append([int(numstring) for numstring in binaryHash])
+    arr = np.array(binaryLists)
+    regions = 0
+    for x in range(128):
+        for y in range(128):
+            if arr[y, x] == 1:
+                removeRegion(arr, [y, x])
+                regions += 1
+    return regions
+
+
+def removeRegion(array, startCoord):
+    coordinates = [startCoord]
+    while coordinates:
+        coord = coordinates.pop()
+        for neighbor in getNeighbors(array, coord):
+            coordinates.append(neighbor)
+        array[coord[0], coord[1]] = 0
+
+
+def getNeighbors(array, coord):
+    neighbors = []
+    for adj in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        try:
+            if coord[0] + adj[0] >= 0 and coord[1] + adj[1] >= 0 and array[coord[0] + adj[0], coord[1] + adj[1]] == 1:
+                neighbors.append([coord[0] + adj[0], coord[1] + adj[1]])
+        except IndexError:
+            pass
+    return neighbors
+
+
+def run(data):
+    print usedSpace(data[0])
+    print findRegions(data[0])
+
+
+if __name__ == "__main__":
+    run(read('inputs/day14.txt'))
